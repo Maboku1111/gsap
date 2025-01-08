1d1608820aef3d6f2f5b27a7b860f9cfdf3775ac
EzrahCodes <112862267+Maboku1111@users.noreply.github.com>
2025-01-08 06:46:05 +0200

    animation modified

--- src/components/FloatAnimation.tsx
+++ src/components/FloatAnimation.tsx
@@ -6,7 +6,11 @@ const FloatAnimation = () => {
   useEffect(() => {
     gsap.to(".float", {
       duration: 1.5,
-      y: 10, // Adjust this for the desired float height
+      y: 30, // Adjust this for the desired float height
+      x: 60,
+      opacity: 1,
+      rotation: "random(-360, 360)",
+      stagger: 0.1,
       ease: "power1.inOut",
       repeat: -1, // Repeat indefinetly
       yoyo: true, // Reverse the animation
